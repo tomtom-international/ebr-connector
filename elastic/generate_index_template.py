343151ffc86951291315a7566ffc23b4a18e08a7
Martin Migasiewicz <martin.migasiewicz@tomtom.com>
2018-12-20 14:31:10 +0100

    feat: add script for generating an index template

---
+++ elastic/generate_index_template.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import json
+import sys
+from elasticsearch_dsl import Index
+from elastic.BuildResults import BuildResults
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Script for generating an index template out of a document")
+    parser.add_argument("INDEX_NAME", help="Name of index")
+    args = parser.parse_args()
+
+    document = BuildResults()
+    index = Index(args.INDEX_NAME)
+    index.document(document)
+    index_template = index.as_template(template_name="template")
+    print(json.dumps(index_template.to_dict(), ensure_ascii=False, indent=4, sort_keys=True))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
