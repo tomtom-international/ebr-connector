8a3f8249c1ddab59ad1019b1bc595e95a64ec847
Martin Migasiewicz <martin.migasiewicz@tomtom.com>
2018-12-21 14:21:20 +0100

    Lowercase project name in url

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email="eugene.davis@tomtom.com",
     description="Library that defines the schema used for pushing test results into Elasticsearch",
     long_description=readme,
-    url=("https://***REMOVED***/projects/NAV/repos/"
+    url=("https://***REMOVED***/projects/nav/repos/"
          "ebr-connector/browse"),
     packages=find_packages(),
     python_requires=">3.5",
