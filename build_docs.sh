13ef181e0c734fa9ad7a56adc55b4ec9908ac17c
Eugene Davis <eugene.davis@tomtom.com>
2019-01-04 14:47:59 +0100

    Add creation of _static folder in script

--- build_docs.sh
+++ build_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir temp_doc
+mkdir temp_doc _static
 sphinx-apidoc --module-first elastic/ -o temp_doc
 es-generate-index-template build-results-schema > temp_doc/schema.json
 sphinx-build -b html . docs
