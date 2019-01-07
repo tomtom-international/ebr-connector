557c7346a30b83e50f3100171fcb3571c95ed621
Eugene Davis <eugene.davis@tomtom.com>
2019-01-07 17:54:27 +0100

    Updated xunit hook

--- elastic/hooks/xunit/store_results.py
+++ elastic/hooks/xunit/store_results.py
@@ -17,7 +17,10 @@ def get_all_xunit_files(testfiles):
         temp_results = get_xunit_results(testfile)
         results['tests'].extend(temp_results['tests'])
         results['suites'].extend(temp_results['suites'])
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
     return results
 
 def get_xunit_results(filename):
@@ -29,6 +32,7 @@ def get_xunit_results(filename):
     }
 
     for suite in xml:
+<<<<<<< Updated upstream
         time = 0
         # Wrap time access in try except due to occasional string times
         try:
@@ -53,11 +57,27 @@ def get_xunit_results(filename):
             except:
                 pass
 
+=======
+        suite_result = {
+            'failuresCount': suite.failures + suite.errors,
+            'skippedCount': suite.skipped,
+            'passedCount': suite.tests - (suite.failures + suite.errors),
+            'totalCount': suite.tests,
+            'name': suite.name,
+            'duration': suite.time
+        }
+        results['suites'].append(suite_result)
+        for case in suite:
+>>>>>>> Stashed changes
             if case.result is None:
                 result = "passed"
                 message = ""
             else:
+<<<<<<< Updated upstream
                 result = case.result._tag
+=======
+                result = case.result.__name__
+>>>>>>> Stashed changes
                 message = case.result.message
 
             test = {
@@ -66,7 +86,11 @@ def get_xunit_results(filename):
                 'test': case.name,
                 'result': result,
                 'message': message,
+<<<<<<< Updated upstream
                 'duration': time
+=======
+                'duration': case.time
+>>>>>>> Stashed changes
             }
 
             results['tests'].append(test)
@@ -81,7 +105,11 @@ def main():
     parser.add_argument('--buildurl', required=True, help='URL of build to send')
     parser.add_argument('--buildtime', required=True, help="Build date-time string")
     parser.add_argument('--buildstatus', required=True, help="Build status string")
+<<<<<<< Updated upstream
     parser.add_argument('--testfiles')
+=======
+    parser.add_argument('--testfiles', nargs='+', required=True, help="List of XML files to parse")
+>>>>>>> Stashed changes
     add_common_args(parser)
     args = parser.parse_args()
 
@@ -91,7 +119,11 @@ def main():
         exit()
 
     xunit_results = BuildResults(jobName=args.jobname, buildId=args.buildid, buildDateTime=args.buildtime, jobLink=args.buildurl)
+<<<<<<< Updated upstream
     xunit_results.store_tests(get_all_xunit_files, {'testfiles': [args.testfiles]})
+=======
+    xunit_results.store_tests(get_all_xunit_files, args.testfiles)
+>>>>>>> Stashed changes
     xunit_results.store_status(status, args)
 
     xunit_results.save_logcollect(args.logcollectaddr, args.logcollectport, cafile=args.cacert, clientcert=args.clientcert, clientkey=args.clientkey,
