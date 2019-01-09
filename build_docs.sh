cde90151e8f3f1c2e89fc15d5d85507eba4b9e0f
Eugene Davis <eugene.davis@tomtom.com>
2019-01-09 16:40:54 +0100

    Removed build_docs now that it is called from setup.py

--- build_docs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir temp_doc _static
-python3 -msphinx.ext.apidoc --module-first elastic/ -o temp_doc
-es-generate-index-template build-results-schema --OUTPUT_FILE temp_doc/schema.json
-python3 -msphinx -b html . docs
