a1b0d6221edb05aeff9a25a588109a800d81cedb
James Robert Mechen <James.Mechen@tomtom.com>
2019-02-05 13:53:15 +0000

    Merge pull request #55 in NAV/elastic-build-results from feature/qbhook_unit_tests to master
    Squashed commit of the following:
    
    commit 8ec29c22f2b64dd7bd713fdcea111697b7cf6765
    Merge: a380ad3 0f2afb9
    Author: James Mechen <james.mechen@tomtom.com>
    Date:   Tue Feb 5 13:24:49 2019 +0000
    
        Merge remote-tracking branch 'origin/master' into feature/qbhook_unit_tests
    
    commit a380ad331161507cbf6d5fe8609bfe92f1fbd1c4
    Author: James Mechen <james.mechen@tomtom.com>
    Date:   Mon Feb 4 13:55:17 2019 +0000
    
        Remove unused import
    
    commit 372b2a3be1cdccce1b4255682e31efafb1bbd738
    Author: James Mechen <james.mechen@tomtom.com>
    Date:   Mon Feb 4 13:51:58 2019 +0000
    
        Updates based on review comments
    
    commit 4060bc5b1d6e681b1d5668b3241c8c465e308e14
    Merge: 7acba9d d7efbc7
    Author: James Mechen <james.mechen@tomtom.com>
    Date:   Mon Feb 4 13:33:43 2019 +0000
    
        Merge remote-tracking branch 'origin/master' into feature/qbhook_unit_tests
    
    commit 7acba9d502f105c3e202def253124b03a88ff617
    Author: James Mechen <james.mechen@tomtom.com>
    Date:   Thu Jan 31 16:04:46 2019 +0000
    
        Remove unused import
    
    commit d54c991595c302f04cf0e66761800d8294d406e3
    Author: James Mechen <james.mechen@tomtom.com>
    Date:   Thu Jan 31 16:00:13 2019 +0000
    
        Add documentation comments for unit tests
    
    commit 26a13855726008faac1e2c5d34cd581d023affc3
    Author: James Mechen <james.mechen@tomtom.com>
    Date:   Thu Jan 31 15:53:08 2019 +0000
    
        Pylint fix
    
    commit 59412fa01daeb6eb4978d9d63741462ae6256808
    Author: James Mechen <james.mechen@tomtom.com>
    Date:   Thu Jan 31 15:49:47 2019 +0000
    
        Add test for status()
    
    commit 972be29d2894dcb93ae45cf9237c3dd9b8f1e90b
    Author: James Mechen <james.mechen@tomtom.com>
    Date:   Thu Jan 31 14:57:07 2019 +0000
    
        Add tests for quickbuild_xml_decode()
    
    commit e6533c23dd028eb46b0075fc459ac7adb5235fd9
    Author: James Mechen <james.mechen@tomtom.com>
    Date:   Thu Jan 31 14:39:28 2019 +0000
    
        Add tests for format_quickbuild_results()

---
+++ test/hooks/quickbuild/test_store_results.py
@@ -0,0 +1,210 @@
+"""
+Tests for the QuickBuild StoreResults module.
+"""
+
+from unittest.mock import MagicMock
+
+import elastic.hooks.quickbuild.store_results as store_results
+from elastic.schema.build_results import BuildResults, Test
+from qb_results_exporter.qb_results_exporter import QBResultsExporter
+
+
+DUMMY_BUILD_INFO_SUCCESS = {
+    QBResultsExporter.KEY_BUILD_ID: '123456',
+    QBResultsExporter.KEY_BUILD_STATUS: 'SUCCESS'
+}
+
+DUMMY_BUILD_INFO_FAILURE = {
+    QBResultsExporter.KEY_BUILD_ID: '654321',
+    QBResultsExporter.KEY_BUILD_STATUS: 'FAILURE'
+}
+
+DUMMY_BUILD_TEST_DATA_SUCCESS = {
+    'Report Set 1': [
+        {
+            QBResultsExporter.KEY_TEST_NAME: 'Test 1',
+            QBResultsExporter.KEY_CLASS_NAME: 'Class 1',
+            QBResultsExporter.KEY_DURATION: 123,
+            QBResultsExporter.KEY_PACKAGE_NAME: 'Package 1',
+            QBResultsExporter.KEY_STATUS: 'PASSED',
+            QBResultsExporter.KEY_ERROR_MESSAGE: None,
+            QBResultsExporter.KEY_REPORT_SET: 'Report Set 1'
+        },
+        {
+            QBResultsExporter.KEY_TEST_NAME: 'Test 2',
+            QBResultsExporter.KEY_CLASS_NAME: 'Class 2',
+            QBResultsExporter.KEY_DURATION: 456,
+            QBResultsExporter.KEY_PACKAGE_NAME: 'Package 2',
+            QBResultsExporter.KEY_STATUS: 'PASSED',
+            QBResultsExporter.KEY_ERROR_MESSAGE: None,
+            QBResultsExporter.KEY_REPORT_SET: 'Report Set 1'
+        }
+    ],
+    'Report Set 2': [
+        {
+            QBResultsExporter.KEY_TEST_NAME: 'Test 3',
+            QBResultsExporter.KEY_CLASS_NAME: 'Class 1',
+            QBResultsExporter.KEY_DURATION: 321,
+            QBResultsExporter.KEY_PACKAGE_NAME: 'Package 1',
+            QBResultsExporter.KEY_STATUS: 'PASSED',
+            QBResultsExporter.KEY_ERROR_MESSAGE: None,
+            QBResultsExporter.KEY_REPORT_SET: 'Report Set 2'
+        }
+    ]
+}
+
+DUMMY_BUILD_TEST_DATA_FAILURE = {
+    'Report Set 1': [
+        {
+            QBResultsExporter.KEY_TEST_NAME: 'Test 1',
+            QBResultsExporter.KEY_CLASS_NAME: 'Class 1',
+            QBResultsExporter.KEY_DURATION: 123,
+            QBResultsExporter.KEY_PACKAGE_NAME: 'Package 1',
+            QBResultsExporter.KEY_STATUS: 'PASSED',
+            QBResultsExporter.KEY_ERROR_MESSAGE: None,
+            QBResultsExporter.KEY_REPORT_SET: 'Report Set 1'
+        },
+        {
+            QBResultsExporter.KEY_TEST_NAME: 'Test 2',
+            QBResultsExporter.KEY_CLASS_NAME: 'Class 2',
+            QBResultsExporter.KEY_DURATION: 456,
+            QBResultsExporter.KEY_PACKAGE_NAME: 'Package 2',
+            QBResultsExporter.KEY_STATUS: 'FAILED',
+            QBResultsExporter.KEY_ERROR_MESSAGE: 'Error 3',
+            QBResultsExporter.KEY_REPORT_SET: 'Report Set 1'
+        }
+    ],
+    'Report Set 2': [
+        {
+            QBResultsExporter.KEY_TEST_NAME: 'Test 3',
+            QBResultsExporter.KEY_CLASS_NAME: 'Class 1',
+            QBResultsExporter.KEY_DURATION: 321,
+            QBResultsExporter.KEY_PACKAGE_NAME: 'Package 1',
+            QBResultsExporter.KEY_STATUS: 'SKIPPED',
+            QBResultsExporter.KEY_ERROR_MESSAGE: None,
+            QBResultsExporter.KEY_REPORT_SET: 'Report Set 2'
+        }
+    ]
+}
+
+
+class MockedQBResultsExporter():
+    """
+    Mocked QBResultsExporter object.
+    """
+    def __init__(self, logger=None, qb_username=None, qb_password=None):
+        self.logger = logger
+        self.qb_username = qb_username
+        self.qb_password = qb_password
+        self.build_test_data = None
+        self.build_id = None
+
+    def set_build_test_data(self, build_test_data):
+        """Set build test data.
+        """
+        self.build_test_data = build_test_data
+
+    def get_build_test_data(self, build_id):
+        """Get previously set build test data and set the given build ID.
+        """
+        self.build_id = build_id
+        return self.build_test_data
+
+    def get_build_id(self):
+        """Get build ID.
+        """
+        return self.build_id
+
+
+def verify_test(test, test_name, suite_name, class_name, result, message, duration, reportset):
+    """Verify that test object has expected field values.
+    """
+    assert test['suite'] == suite_name
+    assert test['classname'] == class_name
+    assert test['test'] == test_name
+    assert test['result'] == result.name
+    assert test['message'] == message
+    assert test['duration'] == duration
+    assert test['reportset'] == reportset
+
+def verify_suite(suite, suite_name, passed_count, failures_count, skipped_count, duration, package):
+    """Verify that suite object has expected field values.
+    """
+    assert suite['failures_count'] == failures_count
+    assert suite['skipped_count'] == skipped_count
+    assert suite['passed_count'] == passed_count
+    assert suite['name'] == suite_name
+    assert suite['total_count'] == failures_count + skipped_count + passed_count
+    assert suite['duration'] == duration
+    assert suite['package'] == package
+
+def verify_results_success(results):
+    """Verify that success results list has expected tests and suites.
+    """
+    tests = results['tests']
+    assert len(tests) == 3
+    verify_test(tests[0], 'Test 1', 'Class 1', 'Class 1', Test.Result.PASSED, None, 123, 'Report Set 1')
+    verify_test(tests[1], 'Test 2', 'Class 2', 'Class 2', Test.Result.PASSED, None, 456, 'Report Set 1')
+    verify_test(tests[2], 'Test 3', 'Class 1', 'Class 1', Test.Result.PASSED, None, 321, 'Report Set 2')
+
+    suites = results['suites']
+    assert len(suites) == 2
+    verify_suite(suites[0], 'Class 1', 2, 0, 0, 123 + 321, 'Package 1')
+    verify_suite(suites[1], 'Class 2', 1, 0, 0, 456, 'Package 2')
+
+def verify_results_failure(results):
+    """Verify that failure results list has expected tests and suites.
+    """
+    tests = results['tests']
+    assert len(tests) == 3
+    verify_test(tests[0], 'Test 1', 'Class 1', 'Class 1', Test.Result.PASSED, None, 123, 'Report Set 1')
+    verify_test(tests[1], 'Test 2', 'Class 2', 'Class 2', Test.Result.FAILED, 'Error 3', 456, 'Report Set 1')
+    verify_test(tests[2], 'Test 3', 'Class 1', 'Class 1', Test.Result.SKIPPED, None, 321, 'Report Set 2')
+
+    suites = results['suites']
+    assert len(suites) == 2
+    verify_suite(suites[0], 'Class 1', 1, 0, 1, 123 + 321, 'Package 1')
+    verify_suite(suites[1], 'Class 2', 0, 1, 0, 456, 'Package 2')
+
+def test_format_quickbuild_results_success():
+    """Test that passing QuickBuild test data is correctly formatted.
+    """
+    results, build_status = store_results.format_quickbuild_results(DUMMY_BUILD_TEST_DATA_SUCCESS)
+    assert build_status == BuildResults.BuildStatus.SUCCESS
+    verify_results_success(results)
+
+def test_format_quickbuild_results_failure():
+    """Test that failing QuickBuild test data is correctly formatted.
+    """
+    results, build_status = store_results.format_quickbuild_results(DUMMY_BUILD_TEST_DATA_FAILURE)
+    assert build_status == BuildResults.BuildStatus.FAILURE
+    verify_results_failure(results)
+
+def test_quickbuild_xml_decode_success():
+    """Test that passing QuickBuild test data is correctly exported.
+    """
+    mock_logger = MagicMock()
+    qb_results_exporter = MockedQBResultsExporter()
+    qb_results_exporter.set_build_test_data(DUMMY_BUILD_TEST_DATA_SUCCESS)
+    results = store_results.quickbuild_xml_decode(DUMMY_BUILD_INFO_SUCCESS, qb_results_exporter, mock_logger)
+    verify_results_success(results)
+    assert qb_results_exporter.get_build_id() == '123456'
+
+def test_quickbuild_xml_decode_failure():
+    """Test that failing QuickBuild test data is correctly exported.
+    """
+    mock_logger = MagicMock()
+    qb_results_exporter = MockedQBResultsExporter()
+    qb_results_exporter.set_build_test_data(DUMMY_BUILD_TEST_DATA_FAILURE)
+    results = store_results.quickbuild_xml_decode(DUMMY_BUILD_INFO_FAILURE, qb_results_exporter, mock_logger)
+    verify_results_failure(results)
+    assert qb_results_exporter.get_build_id() == '654321'
+
+def test_get_status():
+    """Test that correct status is returned.
+    """
+    status = store_results.get_status(DUMMY_BUILD_INFO_SUCCESS)
+    assert status == BuildResults.BuildStatus.SUCCESS.name
+
+    status = store_results.get_status(DUMMY_BUILD_INFO_FAILURE)
+    assert status == BuildResults.BuildStatus.FAILURE.name
